65aa59b83cb8792ca7e80575fe1acd74ee1bf74d
wwj718 <wuwenjie718@gmail.com>
2014-08-13 15:33:04 +0800

    Update views.py

--- skeleton/todo/views.py
+++ skeleton/todo/views.py
@@ -1,9 +1,9 @@
 from models import Todo
 from rest_framework import generics
-from rest_framework.decorators import api_view
-from rest_framework.mixins import DestroyModelMixin
-from rest_framework.reverse import reverse
-from rest_framework.response import Response
+#from rest_framework.decorators import api_view
+#from rest_framework.mixins import DestroyModelMixin
+#from rest_framework.reverse import reverse
+#from rest_framework.response import Response
 from serializers import TodoSerializer
 
 class TodoView(generics.ListCreateAPIView):
